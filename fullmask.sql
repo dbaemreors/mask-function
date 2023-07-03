79047272352bab7fa7e82d5846953d16e6684e44
dbaemreors <132146256+dbaemreors@users.noreply.github.com>
2023-07-03 18:07:34 +0300

    Update fullmask.sql

--- fullmask.sql
+++ fullmask.sql
@@ -13,11 +13,15 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-use:
+/* use:  */
 
+/*   for 1 column:   */
+    
 UPDATE table SET column_name = custom_mask(column_name, '*');
 
 
+/*  for multiple columns:  */
+    
 UPDATE table SET
     column_name1 = custom_mask(column_name1, '*'),
     column_name2 = custom_mask(column_name2, '*'),
